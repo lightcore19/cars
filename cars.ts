9c2b36e463c2795f10eae0b2f84bdaa3f1dbc996
lightcore19 <leestas418@gmail.com>
2025-06-23 22:20:20 +0500

    test

---
+++ cars.ts
@@ -0,0 +1,100 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-cars',
+  imports: [ReactiveFormsModule],
+  templateUrl: './cars.html',
+  styleUrl: './cars.css',
+})
+export class Cars {
+  http = inject(HttpClient);
+  cars: any = [];
+
+  carsFilter = [
+    {
+      active: true,
+      name: 'Все марки',
+    },
+    {
+      active: false,
+      name: 'Lamborghini',
+    },
+    {
+      active: false,
+      name: 'Ferrari',
+    },
+    {
+      active: false,
+      name: 'Porsche',
+    },
+    {
+      active: false,
+      name: 'BMW',
+    },
+    {
+      active: false,
+      name: 'Mercedes',
+    },
+    {
+      active: false,
+      name: 'Chevrolet',
+    },
+    {
+      active: false,
+      name: 'Audi',
+    },
+    {
+      active: false,
+      name: 'Ford',
+    },
+  ];
+
+  orderForm = new FormGroup({
+    car: new FormControl(''),
+    name: new FormControl(''),
+    phone: new FormControl(''),
+  });
+
+  ngOnInit() {
+    this.getCars("");
+  }
+
+  getCars(filter:string){
+    this.http.get("https://testologia.ru/cars-data",{params:{filter:filter}})
+    .subscribe((data) => (this.cars=data));
+  }
+
+  changeFilter(filter: any, carsContent: HTMLElement) {
+    this.carsFilter.forEach((el) => (el.active = false));
+    filter.active = true;
+
+    const filterText = filter.name.toLowerCase();
+
+    this.getCars(filter.name);
+
+    carsContent.scrollIntoView({ behavior: 'instant' });
+  }
+
+  isError(fieldName: string) {
+    const control = this.orderForm.get(fieldName);
+    return !!(control?.invalid && (control?.dirty || control?.touched));
+  }
+
+  sendOrder() {
+    if (this.orderForm.valid) {
+      this.http
+        .post('https://testologia.ru/cars-order', this.orderForm.value)
+        .subscribe({
+          next: (response: any) => {
+            alert(response.message);
+            this.orderForm.reset();
+          },
+          error: (response: any) => {
+            alert(response.error.message);
+          },
+        });
+    }
+  }
+}
